12d0f1aa1cbbe8240d2303f417cfba7f51320c0e
ASelviyan <angelikaselviyan5@gmail.com>
2022-06-08 21:04:55 -0600

    homework completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Calculator extends Component {
+  state = {
+    num1: '',
+    num2: '',
+    total: '',
+    operator: ''
+  }
+
+  //helper methods
+  handleSetNum = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleCalculate = (e) => {
+    e.preventDefault()
+    let num1 = Number(this.state.num1)
+    let num2 = Number(this.state.num2)
+    console.log(num1)
+    // let sum = num1 + num2
+    if (this.state.operator === '+') {
+      this.setState({ total: num1 + num2 })
+    } else if (this.state.operator === '-') {
+      this.setState({ total: num1 - num2 })
+    } else if (this.state.operator === '/') {
+      this.setState({ total: num1 / num2 })
+    } else if (this.state.operator === '*') {
+      this.setState({ total: num1 * num2 })
+    }
+  }
+
+  render() {
+    return (
+      <div className="caclulator">
+        <h1>Calculate with React!</h1>
+
+        <form onSubmit={this.handleCalculate}>
+          <input type="number"
+            name="num1"
+            value={this.state.num1}
+            onChange={this.handleSetNum}
+          />
+          <button
+            onClick={() => {
+              this.setState({ operator: '+' })
+            }}
+          >
+            +
+          </button>
+
+          <button
+            onClick={() => {
+              this.setState({ operator: "-" })
+            }}
+          >
+            -
+          </button>
+
+          <button
+            onClick={() => {
+              this.setState({ operator: "/" })
+            }}
+          >
+            /
+          </button>
+
+          <button
+            onClick={() => {
+              this.setState({ operator: "*" })
+            }}
+          >
+            *
+          </button>
+
+          <input type="number"
+            name="num2"
+            value={this.state.num2}
+            onChange={this.handleSetNum}
+          />
+          <button>=</button>
+          <h3>{this.state.total}</h3>
+        </form>
+      </div>
+    );
+  }
+
 }
 
-export default App;
+export default Calculator;
